dc54991a4712342f87a74fe2b6577ebd7ca8aa8f
Rchik123 <rezichiko@gmail.com>
2022-08-25 13:55:41 +0400

    Updated User.java

--- FinalProject/src/main/java/ObjectClasses/User.java
+++ FinalProject/src/main/java/ObjectClasses/User.java
@@ -6,23 +6,8 @@
 package ObjectClasses;
 
 public class User {
-    private final int user_id;
-    private final String userName;
+    private int userId;
+    private String username;
 
-    public User(int user_id, String userName) {
-        this.user_id = user_id;
-        this.userName = userName;
-    }
 
-    public int getUserId() {
-        return user_id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getLink() {
-        return "";
-    }
 }
